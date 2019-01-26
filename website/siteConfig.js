bb98345019f6938765cce63b836ee3ae6a097c18
Daniel Williams <danielw093@gmail.com>
2019-01-26 12:23:07 +0000

    Small changes to site config

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,7 +23,7 @@
 const siteConfig = {
   title: 'Loris Settings Manager', // Title for your website.
   tagline: 'Documentation',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
+  url: 'https://sharploris.github.io', // Your website URL
   baseUrl: '/LorisSettingsManager/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
@@ -73,7 +73,7 @@ const siteConfig = {
   */
 
   // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright: `Copyright © ${new Date().getFullYear()} Your Name or Your Company Name`,
+  copyright: `Copyright © ${new Date().getFullYear()} Sharp Loris Games`,
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
